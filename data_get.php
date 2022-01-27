2f8bc3545fadfdcee9fb1fd516381cc2e7b9df42
Gunawan Prasetyo <63904116+IDGunawan411@users.noreply.github.com>
2022-01-27 09:56:30 +0700

    update get

--- data_get.php
+++ data_get.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json; charset=utf-8');
 include 'koneksi.php';
 if($_SERVER['REQUEST_METHOD']=="GET"){
-    $sql = mysqli_query($koneksi, "SELECT * FROM data_reminder ORDER BY rTime ASC");
+    $sql = mysqli_query($koneksi, "SELECT * FROM data_reminder WHERE rUserID = '$_GET[uid]' ORDER BY rTime ASC");
     $arrData = [];
     $arrData['allData'] = [];
 
